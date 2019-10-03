1bdcd38634d0b6830cbb364a54b49d089bfb9f73
Smoosh1 <macievskyivan@gmail.com>
2019-10-04 00:12:55 +0300

    dz
    dz

---
+++ for.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For
+{
+    class Program
+    {
+        static void Main()
+        {
+            // for9();
+           // for14();
+        }
+        static void for9()
+        {
+            int a = 2;
+            int b = 5;
+            int sum = 0;
+            for (int i = a; i <= b; i++)
+                sum += i * i;
+            Console.WriteLine(sum);
+        }
+        static void for10()
+        {
+            int n = 4;
+            double znachenie = 0;
+            for (double i = 1; i <= n; i++)           
+                znachenie += 1 / i;
+               
+             Console.WriteLine(znachenie);            
+        }
+        static void for14()
+        {
+            int n = 2;
+            double Kvadrat = 0;
+            for (int i = 1; i <= n * n; i+=2*n-1)
+            {
+               //Kvadrat += 2 * n - 1;
+    
+                Console.WriteLine(i);
+            }
+        }
+
+        static void for15()
+        {
+            double A = 5;
+            int n = 2;
+            double znachenie = 0;
+            for (double i = A; i <= Math.Pow(A,n); i*=n)
+                znachenie += A;
+
+            Console.WriteLine(znachenie);
+        }
+
+
+    }
+}
---
+++ while.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While
+{
+    class Program
+    {
+        static void Main()
+        {
+            while1();
+            //while4();
+        }
+        static void while1()
+        {
+            int A = 4;
+            int B = 2;
+            while (A >= B)
+            {
+                A -=B;
+                Console.WriteLine(A);
+
+            }
+            
+
+        }
+        static void while4()
+        {
+            int n = 9;
+            while (n % 3 == 0)
+                n /= 3;
+            Console.WriteLine(n == 1 ? "True" : "False");
+
+        }
+        static void while5()
+        {
+            int n = 4;
+            int s = 0;
+            while (n % 2 == 0)
+                s += n /= 2;
+            Console.WriteLine(n == 1 ? s++ : s);
+
+        }
+    }
+
+
+}
